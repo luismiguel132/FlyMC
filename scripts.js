7b5e67d9d3dee4e1d9a0fcb6feb3d963eff87d4a
luismiguel132 <marapereira1182@gmail.com>
2024-09-11 22:50:00 -0300

    responcividade para mobile

--- scripts.js
+++ scripts.js
@@ -7,36 +7,26 @@ function entrarServidor() {
         const now = new Date().getTime();
         const distance = countDownDate - now;
 
-        // Cálculo do tempo restante
         const days = Math.floor(distance / (1000 * 60 * 60 * 24));
         const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        // Exibir o tempo restante
         countdownElement.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-        // Quando o temporizador expirar
         if (distance < 0) {
             clearInterval(x);
             countdownElement.innerHTML = "O tempo acabou!";
-            comingSoonElement.style.display = 'none'; // Esconder o texto "EM BREVE"
+            comingSoonElement.style.display = 'none';
             alert("Conecte-se ao servidor FlyMC: mi-01.bedhosting.com.br:25591");
         }
     }, 1000);
 }
 
-// Seleciona o botão de menu e a lista de navegação
-const menuToggle = document.querySelector('.menu-toggle');
+// Seleção de menu
+const menuToggle = document.getElementById('menu-toggle');
 const menu = document.querySelector('nav ul.menu');
 
-// Adiciona o evento de clique ao botão
 menuToggle.addEventListener('click', () => {
-    // Alterna a classe 'active' no menu
     menu.classList.toggle('active');
 });
-
-// Temporizador (exemplo simples de contagem regressiva)
-function entrarServidor() {
-    alert("Conectando ao servidor...");
-}
